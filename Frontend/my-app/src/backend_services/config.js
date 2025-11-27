a076f7588d694b8cb51e0572d4bc3495a4e14a8c
jbs671 <jbs671@mail.usask.ca>
2025-11-27 00:20:56 -0600

    removed /api from config in frontend

--- Frontend/my-app/src/backend_services/config.js
+++ Frontend/my-app/src/backend_services/config.js
@@ -1,7 +1,7 @@
 // services/backend/config.js
 import axios from 'axios';
 
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || '/api';
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BACKEND_URL,
